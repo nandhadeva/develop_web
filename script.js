f657e155842f1c6e2cf09ec95009378b1ad4fcf6
Nandhakumar <93440389+nandhadeva@users.noreply.github.com>
2023-02-09 21:53:34 +0530

    Password

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ loveLink.addEventListener("click", function (event) {
 
   const password = passwordInput.value;
 
-  if (password === "iloveyou" || password === "subash") {
+  if (password === "deva" || password === "subash") {
     window.location.href = "https://projectnn.vercel.app";
   } else {
     alert("Incorrect password.");
