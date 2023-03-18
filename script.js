5376d166c69ed308803203ad8daedef86858ffa0
Nandhakumar <93440389+nandhadeva@users.noreply.github.com>
2023-03-18 20:01:56 +0530

    love_link_added

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ loveLink.addEventListener("click", function (event) {
   const password = passwordInput.value;
 
   if (password === "deva_" || password === "subash") {
-    window.location.href = "https://tenor.com/en-IN/view/gpmuthu-sethapayale-nakku-swearing-tiktok-gif-20326152";
+    window.location.href = "https://media.tenor.com/bIrHe_MGkQAAAAAd/gpmuthu-sethapayale.gif";
   } else {
     alert("Incorrect password.");
   }
